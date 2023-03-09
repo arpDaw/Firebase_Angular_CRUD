0190bd572d77fa1b09ff532050f3415084b57f88
Pablo <pabloruizalfaro@gmail.com>
2023-03-09 16:29:56 +0100

    lista

--- pokecrud/src/app/features/pokemon/pokemon.component.ts
+++ pokecrud/src/app/features/pokemon/pokemon.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, filter, tap } from 'rxjs';
+
+import { FormComponent } from './components/form/form.component';
+import { MatDialog } from '@angular/material/dialog';
+import { PokedexFirestoreService } from 'src/app/core/pokedex-firestore.service';
+import { Pokemon } from './interfaces/pokemon.interface';
 
 @Component({
   selector: 'app-pokemon',
   templateUrl: './pokemon.component.html',
   styleUrls: ['./pokemon.component.scss']
 })
-export class PokemonComponent {
+export class PokemonComponent implements OnInit{
+  allPokemon$: Observable<Pokemon[]>;
+  selectedPokemon?: Pokemon;
 
+  constructor(
+    private readonly pokedexService: PokedexFirestoreService,
+    private readonly dialog: MatDialog
+  ) {}
+
+  ngOnInit(): void {
+      this.allPokemon$ = this.pokedexService.getAll();
+  }
+
+  addPokemon(){
+    const dialogRef = this.dialog.open(FormComponent, {
+      data: {},
+      width: '40%',
+    });
+
+    dialogRef
+      .afterClosed()
+      .pipe(
+        filter(Boolean),
+        tap((pokemon) => this.pokedexService.create(pokemon))
+      ).subscribe();
+  }
+
+  updatePokemon(){
+    const dialogRef = this.dialog.open(FormComponent, {
+      data: {...this.selectedPokemon },
+      width: '40%',
+    });
+
+    dialogRef
+      .afterClosed()
+      .pipe(
+        filter(Boolean),
+        tap((pokemon)=> this.pokedexService.update(pokemon)),
+        tap((pokemon)=> this.selectPokemon(pokemon))
+      ).subscribe();
+  }
+
+  selectPokemon(pokemon: Pokemon){
+    this.selectedPokemon = pokemon;
+  }
+
+  deletePokemon(){
+    this.pokedexService.delete(this.selectedPokemon!.id);
+    this.selectedPokemon = undefined;
+  }
 }
--- pokecrud/src/app/features/pokemon/pokemon.module.ts
+++ pokecrud/src/app/features/pokemon/pokemon.module.ts
@@ -10,12 +10,18 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ListComponent } from './components/list/list.component';
+
+import { MatCardModule } from '@angular/material/card';
+import { DetailComponent } from './components/detail/detail.component';
 
 
 @NgModule({
   declarations: [
     PokemonComponent,
-    FormComponent
+    FormComponent,
+    ListComponent,
+    DetailComponent
   ],
   imports: [
     CommonModule,
@@ -24,7 +30,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatDialogModule
+    MatDialogModule,
+    MatCardModule
   ]
 })
 export class PokemonModule { }
