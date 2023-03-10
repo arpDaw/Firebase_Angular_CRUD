7dc540c763bd5235d1fd7c9bb542be09c5be2243
Pablo <pabloruizalfaro@gmail.com>
2023-03-10 14:32:46 +0100

    tutorial finalizado

---
+++ tutorial1/src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'crud-b89cc',
+    appId: '1:1039731360747:web:25599c2d0c53de26be8fae',
+    storageBucket: 'crud-b89cc.appspot.com',
+    apiKey: 'AIzaSyAd1hc7n2a_j9-FMPJV_5lZW049yhYVzGQ',
+    authDomain: 'crud-b89cc.firebaseapp.com',
+    messagingSenderId: '1039731360747',
+  },};
